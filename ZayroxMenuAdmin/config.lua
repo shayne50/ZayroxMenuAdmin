8c89c5b3a624fc762e3734bb681174845b911bd1
Shayne <paulfavray226@gmail.com>
2025-09-11 01:06:23 +0200

    Update config.lua

--- ZayroxMenuAdmin/config.lua
+++ ZayroxMenuAdmin/config.lua
@@ -4,14 +4,14 @@ Config = {
     --Locale = 'fr', -- Language available: fr, soon
     
     webhook = { -- LOGS DISCORD
-        Staffmodeon = "https://discord.com/api/webhooks/1406723196631257088/ElqBzbglocJCjK0XeHziw8O2NbTKP6RhtAWvv5jLB_OnuM09yXrE05n7f3D09zw6SgBx",      -- MODE STAFF ON
-        Staffmodeoff = "https://discord.com/api/webhooks/1406723489523564555/7gFe8-__9ESvyfz_EFCe8aMfw9SfcT-9ATfh28UGdxcuR2gTesafuth8iBPuQl0LLmBh",     -- MODE STAFF OFF
-        teleport = "https://discord.com/api/webhooks/1406724099828351158/3VP56kJn7liTrB_vlcrQCWTmL8QNVlGeLem-xEm5ZnsCkod7BiCs_1tHcbs7xdVXH-Wv",        -- BRING LOGS
-        teleportTo = "https://discord.com/api/webhooks/1406724219177275575/zIvEQRfIkNkG3VQDsbuSZzLGIGF400KAREM65rMe0abJ5yJbCsAXHW7zadh7lqjKg8L0",         -- GOTO LOGS
-        revive = "https://discord.com/api/webhooks/1406724437637857501/yvOk6Lnj6eGGauAMAgsMaDmi8Du1CniHu7vMRZ05bBcS6DER-EmdCG3K2ghjs3EHdUGL",        -- REVIVE LOGS
-        kick = "https://discord.com/api/webhooks/1406724589333254226/-FLbdaQVsIjlEiama-cvhfZ2P3uT_IotkKnSRnH72dUtwQQltTiHTmDirSMccUEO8E34",        -- KICK/BAN LOGS
-        SendLogs = "https://discord.com/api/webhooks/1406722939533000785/675IuDY2VuyG5ucnxT3PSzGt1_WUt5d-LqjaC2lNpAYuwhl57J6ZIpJSiur8LNaphVlH",        -- GENERAL LOGS
-        report = "https://discord.com/api/webhooks/1406724706605863133/sCCstShqy26x8hBuP6LsqrRfSw2wVO0vHxQJKe76UW2pTvswsnw94YoQVuXjpDyxsTxp"        -- REPORT LOGS
+        Staffmodeon = "",      -- MODE STAFF ON
+        Staffmodeoff = "",     -- MODE STAFF OFF
+        teleport = "",        -- BRING LOGS
+        teleportTo = "",         -- GOTO LOGS
+        revive = "",        -- REVIVE LOGS
+        kick = "",        -- KICK/BAN LOGS
+        SendLogs = "",        -- GENERAL LOGS
+        report = ""        -- REPORT LOGS
     },
 
     Grade = { -- Nom des grades
